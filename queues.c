50c772d4d6c5c4cd7bf1c48c533551439a3541e2
LuutuGregoryValentino <luutugregory@gmail.com>
2026-04-29 20:26:08 +0300

    queues intro

---
+++ queues.c
@@ -0,0 +1,22 @@
+
+
+#include <stdio.h>
+#include <stdlib.h> //malloc free
+
+typedef struct Node
+{
+    int num;
+    struct Node * next;
+} node;
+
+
+node * front = NULL;
+node * rear = NULL;
+
+node * 
+
+
+
+
+
+
