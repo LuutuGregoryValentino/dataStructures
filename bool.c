f25964a9ac937b42796be5f234108c6f15fac2c9
LuutuGregoryValentino <luutugregory@gmail.com>
2026-04-29 20:15:55 +0300

    learning about booleans in C

---
+++ bool.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    bool x;
+    x = true;
+
+    if (x)
+    {
+        printf("x is true\n");
+    }
+}
+
+// int plusOne(int x)
+// {
+//     x++;
+//     return x;
+// }
+// void main(void)
+// {
+//     int y = 3;
+//     y = plusOne(y);
+//     printf("% d", y);
+// }
