376e115559e9d0a89ef4d30aacf85ab639e1ea24
LuutuGregoryValentino <luutugregory@gmail.com>
2026-04-29 20:17:38 +0300

    my first C Program

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i; 
+    i =2;
+
+    float f; 
+    f = 3.14159;
+
+    char *s; 
+    s = "hello, world!";
+    
+    printf("%s, int = %d, float = %f, bool = ",s,i,f);
+    
+}
